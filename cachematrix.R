11e7bd90a8c7c67b61b68d611bec1e6e6f637f13
LAXMISAGAR SAMAI <laxmisagar304@gmail.com>
2019-09-29 01:10:14 +0530

    Assignment Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## In makeCacheMatrix Using get and set functions, I try to incert a matrix into the function and store its inverse through solve() function in "m". In the other function, the 1st (makeCacheMatrix) is called so that the user provided matrix is compared with the invarse that has already been cached, if not then the invarse is calculated seperately and displayed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+set = function(y) {
+x <<- y
+m <<- NULL
+}
+get = function() {
+ x
+}
+setinvarse = function(x){
+m <<- solve(x)
+}
+getinvarse = function() {
+m
+}
+list(set = set, get = get,
+setinvarse = setinvarse,
+getinvarse = getinvarse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Takes an input from the user and provides its invarse, if the cache is already calculated and stored in the cache "m", then returns it, or calculates.
+cacheSolve <- function(y, ...) {
+x=makeCacheMatrix(y)
+m <- x$getinvarse()
+if(!is.null(m)) {
+message("getting cached data")
+return(m)
 }
+data <- x$get()
+m <- solve(data)
+x$setinvarse(m)
+return(m)
+}
+
